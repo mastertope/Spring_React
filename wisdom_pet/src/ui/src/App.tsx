f814c9686d9e2e811f3faed02e6bb735c17864bc
mastertope <tupengabs@gmail.com>
2025-09-21 04:05:47 +0800

    Creation of Home page and initial routes

--- wisdom_pet/src/ui/src/App.tsx
+++ wisdom_pet/src/ui/src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Layout />} >
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      </div>
   )
 }
 
